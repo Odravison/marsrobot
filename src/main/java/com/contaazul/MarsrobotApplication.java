34f29bad1b420c275f00a4d4a599bf657facee9e
Odravison Amaral <odravison.amaral@dce.ufpb.br>
2017-02-01 17:34:33 -0300

    [GITIGNORE] Adds file on gitignore
    * add gitignore
    * addin comment on Main

--- src/main/java/com/contaazul/MarsrobotApplication.java
+++ src/main/java/com/contaazul/MarsrobotApplication.java
@@ -1,12 +1,23 @@
 package com.contaazul;
 
+import com.contaazul.dao.RobotRepository;
+import com.contaazul.models.Orientation;
+import com.contaazul.models.Robot;
+import com.contaazul.utils.Tuple;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MarsrobotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MarsrobotApplication.class, args);
-	}
+    @Autowired
+    private RobotRepository robotRepository;
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MarsrobotApplication.class, args);
+
+    }
 }
