f66b1703833151396bc79afad6f37c0731f0909a
Odravison <odravison.amaral@dce.ufpb.br>
2017-02-01 11:09:18 -0300

    [Exception] Adding exceptions

---
+++ src/main/java/com/contaazul/exceptions/BadMovementException.java
@@ -0,0 +1,11 @@
+package com.contaazul.exceptions;
+
+/**
+ * Created by odravison on 31/01/17.
+ */
+public class BadMovementException extends Exception{
+
+    public BadMovementException (String message){
+        super(message);
+    }
+}
